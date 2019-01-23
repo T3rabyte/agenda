8857b7a40dbc0f6e8c6957bfac1c2445ba57599e
T3rabyte <teun.bruin@hotmail.com>
2019-01-23 14:49:53 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,72 @@
+import java.util.*;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int menu;
+        System.out.println("\nWelkom bij Teun's agenda, \n\n" +
+                "in deze agenda kan je afspraken & deadlines  \n" +
+                "door het de leter voor uw keuze te typen komt u op de bijbehorende plek\n\n" +
+                "1. toevoegen afspraak\n" +
+                "2. toevoegen deadline\n" +
+                "3. bewerken afspraak\n" +
+                "4. bewerken deadline\n" +
+                "5. deadline gehaald / niet gehaald\n" +
+                "6. verwijderen afspraak\n" +
+                "7. verwijderen deadline\n" +
+                "8. weergeven afspraken en deadlines van specifieke dag\n" +
+                "9. weergeven afspraken en deadlines van vandaag\n" +
+                "10. weergeven afspraken en deadlines van specifieke week\n" +
+                "11. weergeven afspraken en deadlines van deze week");
+
+        Scanner input = new Scanner(System.in);
+        menu = input.nextInt();
+
+        if (menu == 1)
+        {
+            afspraken.toevoegen_afspraak();
+        }
+        if (menu == 2)
+        {
+
+        }
+        if (menu == 3)
+        {
+
+        }
+        if (menu == 4)
+        {
+
+        }
+        if (menu == 5)
+        {
+
+        }
+        if (menu == 6)
+        {
+
+        }
+        if (menu == 7)
+        {
+
+        }
+        if (menu == 8)
+        {
+
+        }
+        if (menu == 9)
+        {
+
+        }
+        if (menu == 10)
+        {
+
+        }
+        if (menu == 11)
+        {
+
+        }
+
+
+    }
+}
---
+++ src/afspraken.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class afspraken
+{
+    static List<String> afspraken_String = new ArrayList<>();
+    static List<Integer> afspraken_dag = new ArrayList<>();
+    static List<Integer> afspraken_week = new ArrayList<>();
+
+public static void toevoegen_afspraak()
+        {
+            String toevoegen_string_afspraak;
+            int toevoegen_dag_afspraak;
+            int toevoegen_week_afspraak;
+
+            String print;
+            int print2;
+            int print3;
+
+            System.out.println("Type hier de beschrijving die u bij de afspraak wilt");
+            Scanner input = new Scanner(System.in);
+            toevoegen_string_afspraak = input.nextLine();
+
+            System.out.println("Op welke dag moet de afspraak komen te staan?");
+            toevoegen_dag_afspraak = input.nextInt();
+
+            System.out.println("In welke week valt deze afspraak?");
+            toevoegen_week_afspraak = input.nextInt();
+
+            afspraken_String.add(toevoegen_string_afspraak);
+            afspraken_dag.add(toevoegen_dag_afspraak);
+            afspraken_week.add(toevoegen_week_afspraak);
+        }
+}
